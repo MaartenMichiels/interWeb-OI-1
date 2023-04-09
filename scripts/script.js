6d62d84e4c3753f1afabc92c4dcd07517fc78ce0
maarten michiels <r0955033@student.thomasmore.be>
2023-04-09 21:00:17 +0200

    verbergen van alerts en meer validatie werk

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,43 @@
 // methodes
 // ik gebruik liever geen arrow functions omdat ik sneller de standaard function kan aanmaken met de auto complete van VSCode
-function ValidateFieldForEmpty(field) {
+
+//geef geen message mee omdat deze dynamisch word aangemaakt hier 
+function CheckEmptyField(field) {
     if(field.value == "") return `veld ${field.getAttribute("id")} is vereist!`;
     return "";
 }
-function ValidateAllForEmpty(fields = []) {
+
+function CheckEmptyAll(fields = []) {
     let errors = [];
     fields.forEach(element => {
-        if(ValidateFieldForEmpty(element) != "") errors.push(ValidateFieldForEmpty(element))
+        if(CheckEmptyField(element) != "") errors.push(CheckEmptyField(element))
     });
     
     return errors
+}
+
+function CheckPassword(password,repeat) {
+    
+    let input = [password,repeat]
+    let output = []
+    output = CheckEmptyAll(input)
+
+    if(output.length == 0)
+    {
+        if(password.value.length <= 7)
+        output.push("wachtwoord moet minstens 7 characters bevatten.")
+        if(repeat.value != password.value)
+        output.push("wachtwoord en herhaal wachtwoord zijn niet gelijk")
+    }
+    return output
+}
+
+function HideUIElement(uiElement){
+    uiElement.hidden = true
+}
+
+function ShowUIElement(uiElement) {
+    uiElement.hidden = false
 }
 
 //source https://www.w3resource.com/javascript/form/email-validation.php
@@ -18,23 +45,31 @@ function ValidateEmail(inputText)
 {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(inputText.value.match(mailformat))
-            {
-            alert("Valid email address!");
-            return true;
-            }
+    {
+        return true;
+    }      
     else
-        {
-        alert("You have entered an invalid email address!");
+    {
         return false;
-        }
+    }
 }
+
+//main
+
+const errorAlert = document.querySelector("#errorAlert")
+const confirmAlert = document.querySelector("#confirmAlert")
+const paymentAlert = document.querySelector("#paymentAlert")
+
+HideUIElement(errorAlert)
+HideUIElement(confirmAlert)
+HideUIElement(paymentAlert)
 
 //form declareren
 const form = document.querySelector("#forma")
 
 //click event 
 form.addEventListener("submit", (e) =>{
-    //voorkomen dat de pagina vernieuwd (ik weet dat dit ook voorkomt dat de info van het formulier word verzonden maar dat is niet het doel van deze opdracht)
+    //voorkomen dat de pagina vernieuwd (ik weet dat dit ook voorkomt dat de info van het formulier word verzonden)
     e.preventDefault();
 
     let txtVoornaam = document.querySelector("#voornaam")
@@ -55,12 +90,23 @@ form.addEventListener("submit", (e) =>{
     let rbBetaling = document.querySelectorAll(".paymentMethod")
 
     let errorlog = document.querySelector("#errorlog")
+    // lege error array declareren
     let errors = [];
+    //array met velden die op leeg moeten getest worden
     fieldsForEmpty = [txtVoornaam,txtAchternaam,txtGebruikernaam,txtAdress,cmbLand,cmbProv]
     
-    errors =  ValidateAllForEmpty(fieldsForEmpty)
-    // console.log(errors)
-    // console.log(errorlog)
-    let errorString = errors.toString().replaceAll(",","\n");
-    errorlog.value = errorString
+    //testen voor empty fields -ww velden die worden later gecontroleerd in hun eigen methode
+    errors =  CheckEmptyAll(fieldsForEmpty)
+    
+    //email controleren
+    if(txtEmail.value == null || ValidateEmail(txtEmail) == false)
+    errors.push("E-mailaders is niet correct");
+
+    //wachtwoord controle
+    //deze methode is om te voorkomen dat alle 4 mogelijke errors voor WW tegelijk te zien zijn
+    errors = errors.concat(CheckPassword(txtWachtwoord,txtHerhaalwoord))
+
+    
+    // error log posten    
+    errorlog.innerHTML = errors.toString().replaceAll(",","<br>");
 },false);
