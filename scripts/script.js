578e7922dfba9e9bb4d74a6389c496db33376346
maarten michiels <r0955033@student.thomasmore.be>
2023-04-10 17:05:07 +0200

    laatste checks toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -32,13 +32,26 @@ function CheckPassword(password,repeat) {
     return output
 }
 
+//methodes waar ik een van de alerts kan meegeven en laten zien of verstoppen
 function HideUIElement(uiElement){
     uiElement.hidden = true
 }
-
 function ShowUIElement(uiElement) {
     uiElement.hidden = false
 }
+
+//betalingswijze 
+function CheckRB(fields) {
+     let betalinglog = document.querySelector("#betalinglog")
+     betalinglog.textContent = `Je betalingswijze is ${fields.find(e => e.checked == true).value}`
+}
+
+//postcode word hier niet op leeg gecontroleerd dat word door de CheckEmptyAll methode gedaan
+function CheckPC(field) {
+    if(field.value != "" &&(field.value < 1000 || field.value >10000))
+    return "postcode moet tussen 1000 en 9999 liggen"
+}
+
 
 //source https://www.w3resource.com/javascript/form/email-validation.php
 function ValidateEmail(inputText)
@@ -56,13 +69,14 @@ function ValidateEmail(inputText)
 
 //main
 
+//hide de alerts on startup
 const errorAlert = document.querySelector("#errorAlert")
 const confirmAlert = document.querySelector("#confirmAlert")
 const paymentAlert = document.querySelector("#paymentAlert")
 
-HideUIElement(errorAlert)
-HideUIElement(confirmAlert)
-HideUIElement(paymentAlert)
+// HideUIElement(errorAlert)
+// HideUIElement(confirmAlert)
+// HideUIElement(paymentAlert)
 
 //form declareren
 const form = document.querySelector("#forma")
@@ -84,17 +98,17 @@ form.addEventListener("submit", (e) =>{
     let cmbProv = document.querySelector("#provincie")
     let cmbPost = document.querySelector("#postcode")
 
-    let cbBrief = document.querySelector("#nieuwsbrief")
     let cbVoorwaarden = document.querySelector("#voorwaarden")
-
-    let rbBetaling = document.querySelectorAll(".paymentMethod")
+    // rb betalingen aanmaken als array niet als een nodelist zodat ik de array.find kan gebruiken om de waarden op te vragen
+    let rbBetaling= Array.from(document.querySelectorAll(".paymentMethod"))
+    
 
     let errorlog = document.querySelector("#errorlog")
     // lege error array declareren
     let errors = [];
+
     //array met velden die op leeg moeten getest worden
-    fieldsForEmpty = [txtVoornaam,txtAchternaam,txtGebruikernaam,txtAdress,cmbLand,cmbProv]
-    
+    fieldsForEmpty = [txtVoornaam,txtAchternaam,txtGebruikernaam,txtAdress,cmbLand,cmbProv,cmbPost]    
     //testen voor empty fields -ww velden die worden later gecontroleerd in hun eigen methode
     errors =  CheckEmptyAll(fieldsForEmpty)
     
@@ -105,8 +119,27 @@ form.addEventListener("submit", (e) =>{
     //wachtwoord controle
     //deze methode is om te voorkomen dat alle 4 mogelijke errors voor WW tegelijk te zien zijn
     errors = errors.concat(CheckPassword(txtWachtwoord,txtHerhaalwoord))
-
     
-    // error log posten    
-    errorlog.innerHTML = errors.toString().replaceAll(",","<br>");
+    //postcode check met if statement om een null result uit de error array te houden
+    if(CheckPC(cmbPost) != null)errors.push(CheckPC(cmbPost))
+
+    //betalingswijze 
+    CheckRB(rbBetaling)
+
+    if(cbVoorwaarden.checked == false) errors.push("je moet de algemene voorwaarden accepteren")
+
+    //check ofdat er errors zijn en de juiste UI elementen laten zien
+    if(errors.length == 0)
+    {
+        ShowUIElement(paymentAlert)
+        ShowUIElement(confirmAlert)
+        HideUIElement(errorAlert)   
+    }
+    else
+    {
+        HideUIElement(paymentAlert)
+        HideUIElement(confirmAlert)
+        ShowUIElement(errorAlert)   
+        errorlog.innerHTML = errors.toString().replaceAll(",","<br>");
+    }
 },false);
